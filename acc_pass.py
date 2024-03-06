7bb3870492e0fa37b32ed8562acb3e5b5f1658be
VQHieu1012 <114791661+VQHieu1012@users.noreply.github.com>
2024-03-06 19:56:50 +0700

    update variables

--- acc_pass.py
+++ acc_pass.py
@@ -1,4 +1,12 @@
-ACCOUNT = 'your fb account'
-PASS = 'your fb password'
-USER_DATA_DIR = 'your user data directory'
+## Facebook info
+ACCOUNT = '' # your account
+PASS = ''     # your password
+
+USER_DATA_DIR = '' # path to chrome user data
 ## C:\\Users\\admin\\AppData\\Local\\Google\\Chrome\\User Data\\
+
+## Set up database
+HOST_DB = ''   # database host (example: localhost)
+NAME_DB = ''     # database name (example: data_mining)
+USER_DB = ''    # database user (example: postgres)
+PASSWORD_DB = ''    # user's password 
